5d97ed2b0af9a373edcaf4bf7b3d486ffffb93c8
mintpark <greatmint8@gmail.com>
2019-06-26 10:41:03 +0900

    install Cocoapod, apply SnapKit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+  use_frameworks!
+
+target 'Rabit' do
+
+  pod 'SnapKit', '~> 5.0.0'
+
+end
---
+++ Pods/Target Support Files/Pods-Rabit/Pods-Rabit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rabit : NSObject
+@end
+@implementation PodsDummy_Pods_Rabit
+@end
