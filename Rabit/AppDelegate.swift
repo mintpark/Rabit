8da2ca13ee5fa9500768640a43b4a3623222a137
mintpark <greatmint8@gmail.com>
2019-07-01 10:59:11 +0900

    remove entry point in info.plist and Main.storyboard

--- Rabit/AppDelegate.swift
+++ Rabit/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let mainVC = MainViewController(nibName: MainViewController.className(), bundle: nil)
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
         
         return true
     }
