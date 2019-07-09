aea41cb26d6fb56f12beec8a2a458dba8a77d9a8
mintpark <greatmint8@gmail.com>
2019-07-09 10:55:24 +0900

    add DateViewModel, refactor DATE_COUNT into viewModel

--- Rabit/View/MainTableHeaderView.swift
+++ Rabit/View/MainTableHeaderView.swift
@@ -11,14 +11,13 @@ import SnapKit
 
 final class MainTableHeaderView: UITableViewHeaderFooterView {
     static let height: CGFloat = 44
+    static var viewModel = DateViewModel(Date())
     
     private var stackView: UIStackView = {
-        let DATE_COUNT = 5
-        
-        let labels: [UILabel] = (0..<DATE_COUNT).map { (i) -> UILabel in
+        let labels: [UILabel] = (0..<DateViewModel.DATE_COUNT).map { (i) -> UILabel in
             var label = UILabel(frame: .zero)
             label.textAlignment = .center
-            label.text = String(i)
+            label.text = String(viewModel.days[safe: i]?.day ?? 0)
             return label
         }
         
@@ -27,7 +26,7 @@ final class MainTableHeaderView: UITableViewHeaderFooterView {
         stack.distribution = UIStackViewDistribution.fillEqually
         stack.spacing = 0
         stack.alignment = .center
-        (0..<DATE_COUNT).forEach({ (i) in
+        (0..<DateViewModel.DATE_COUNT).forEach({ (i) in
             stack.addArrangedSubview(labels[safe: i] ?? UILabel(frame: .zero))
         })
         
@@ -48,4 +47,37 @@ final class MainTableHeaderView: UITableViewHeaderFooterView {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+extension MainTableHeaderView {
+    struct HeaderDate {
+        var dateOfWeek: String = "Sun"
+        var day: Int = 1
+        var isSelected: Bool = false
+    }
+
+    struct DateViewModel {
+        static let DATE_COUNT = 5
+        
+        var year: Int = 0
+        var month: Int = 0
+        var days: Array<HeaderDate> = []
+        
+        init(_ date: Date) {
+            let calendar = Calendar.current
+            let todayComponents = calendar.dateComponents([.month, .year], from: date)
+            year = todayComponents.year ?? 0
+            month = todayComponents.month ?? 0
+
+            var dateArray = Array<HeaderDate>()
+            (0..<DateViewModel.DATE_COUNT).forEach { (index) in
+                let dayOfWeekString = ["", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+                let date = Date(timeIntervalSinceNow: TimeInterval(60*60*24*(index-2)))
+                let components = calendar.dateComponents([.weekday, .day], from: date)
+                let dayOfWeek = dayOfWeekString[components.weekday ?? 0]
+                dateArray.append(.init(dateOfWeek: dayOfWeek, day: components.day ?? 1, isSelected: false))
+            }
+            days = dateArray
+        }
+    }
 }
