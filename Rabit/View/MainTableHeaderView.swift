ece493fae8f2b85255eac82781e8b5f6727044b6
mintpark <greatmint8@gmail.com>
2019-08-02 23:50:13 +0900

    fix: swap dayOfWeek and day with re-naming

--- Rabit/View/MainTableHeaderView.swift
+++ Rabit/View/MainTableHeaderView.swift
@@ -21,7 +21,7 @@ final class MainTableHeaderView: UITableViewHeaderFooterView {
         
         dateViews = (0..<MainTableHeaderView.DateViewModel.DATE_COUNT).map ({ (i) in
             let date = MainTableHeaderView.viewModel.days[safe: i]
-            return DateView(day: String(date?.day ?? 1), date: date?.dateOfWeek ?? "")
+            return DateView(dayOfWeek: date?.dayOfWeek ?? "", day: date?.day ?? 0)
         })
         
         stackView = UIStackView(arrangedSubviews: dateViews)
@@ -42,19 +42,20 @@ final class MainTableHeaderView: UITableViewHeaderFooterView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /**
     #warning("DateViewModel 구조 바꿔서 date 하나로 업데이트 칠 수 있어야함")
     func update(_ date: Date) {
         (0..<DateViewModel.DATE_COUNT).forEach { (i) in
             let date = MainTableHeaderView.viewModel.days[i]
             let dateView = dateViews[i]
-            dateView.update(day: String(format: "%d", date.day), date: date.dateOfWeek)
         }
     }
+     */
 }
 
 extension MainTableHeaderView {
     struct HeaderDate {
-        var dateOfWeek: String = "Sun"
+        var dayOfWeek: String = "Sun"
         var day: Int = 1
         var isSelected: Bool = false
     }
@@ -77,46 +78,46 @@ extension MainTableHeaderView {
                 let date = Date(timeIntervalSinceNow: TimeInterval(60*60*24*(index-2)))
                 let components = calendar.dateComponents([.weekday, .day], from: date)
                 let dayOfWeek = dayOfWeekString[components.weekday ?? 0]
-                days.append(.init(dateOfWeek: dayOfWeek, day: components.day ?? 1, isSelected: false))
+                days.append(.init(dayOfWeek: dayOfWeek, day: components.day ?? 1, isSelected: false))
             }
         }
     }
     
     private class DateView: UIView {
-        private let HEIGHT_DAY: CGFloat = 30
+        private let HEIGHT_DAYOFWEEK: CGFloat = 30
         private let HEIGHT_DATE: CGFloat = 60
         
         private var dayLabel: UILabel!
         private var dateButton: UIButton!
         
-        init(day: String, date: String) {
+        init(dayOfWeek: String, day: Int) {
             super.init(frame: .zero)
             
             dayLabel = UILabel()
-            dayLabel.text = day
+            dayLabel.text = dayOfWeek
             dayLabel.textAlignment = .center
             
             dateButton = UIButton()
-            dateButton.setTitle(date, for: .normal)
+            dateButton.setTitle("\(day)", for: .normal)
             dateButton.setTitleColor(.black, for: .normal)
             dateButton.addTarget(self, action: #selector(dateButtonTapped), for: .touchUpInside)
             
             addSubview(dayLabel)
             dayLabel.snp.makeConstraints { (make) in
                 make.top.leading.trailing.equalToSuperview()
-                make.height.equalTo(HEIGHT_DAY)
+                make.height.equalTo(HEIGHT_DAYOFWEEK)
             }
             
             addSubview(dateButton)
             dateButton.snp.makeConstraints { (make) in
-                make.top.equalToSuperview().offset(HEIGHT_DAY)
+                make.top.equalToSuperview().offset(HEIGHT_DAYOFWEEK)
                 make.leading.trailing.equalToSuperview()
                 make.height.equalTo(HEIGHT_DATE)
             }
         }
         
         override var intrinsicContentSize: CGSize {
-            return CGSize(width: 0, height: HEIGHT_DAY+HEIGHT_DATE)
+            return CGSize(width: 0, height: HEIGHT_DAYOFWEEK + HEIGHT_DATE)
         }
         
         required init?(coder aDecoder: NSCoder) {
@@ -125,10 +126,6 @@ extension MainTableHeaderView {
         
         @objc func dateButtonTapped() {
             dateButton.backgroundColor = .green
-        }
-        
-        func update(day: String, date: String) {
-            // update label
         }
     }
 }
